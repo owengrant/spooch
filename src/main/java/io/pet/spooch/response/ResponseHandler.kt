45f8692c5c99231c963a01cc74cd7f56a0f80a0c
Owen Grant <owen.dalmon.grant@gmail.com>
2019-03-18 10:30:04 -0400

    eventbus handlers for eventually handling responses

--- src/main/java/io/pet/spooch/response/ResponseHandler.kt
+++ src/main/java/io/pet/spooch/response/ResponseHandler.kt
@@ -1,4 +1,23 @@
 package io.pet.spooch.response
 
-class ResponseHandler {
+import io.vertx.core.eventbus.Message
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.RoutingContext
+
+open class ResponseHandler {
+     val rCtx = mutableMapOf<String,RoutingContext>()
+
+    open fun handle(message: Message<JsonObject>) {
+        val hash = message.headers().get("hash")
+        val err = message.headers().get("err")
+        val con = rCtx.getOrDefault(hash, null)
+        if(con != null){
+            if(err.isEmpty()){
+                con.response().end(message.body().encode())
+            } else con.fail(500)
+            rCtx.remove(hash)
+        }
+    }
+
+    fun put(hash: String, ctx: RoutingContext) = rCtx.put(hash, ctx)
 }
